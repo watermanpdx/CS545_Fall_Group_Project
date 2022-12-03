5b4e62419b196cbd614896f4ca0dfa915cb3e3a8
watermanpdx <tayte@pdx.edu>
2022-12-02 19:52:39 -0800

    Update (major)
    Removed data_management.py file + classes. Replaced with embedded sklearn functionality + pandas. (Removed dimensionality reduction plotting, may be added back in but not useful atm)
    
    Replaced dataset with spambase due to issues with breast-cancer data. High correlation with each feature, resulted in sharp increase in performance despite model hyperparameters

--- source/main.py
+++ source/main.py
@@ -1,18 +1,11 @@
-"""
-======================================================================
-Description:  Main - main script (tbd)
-Purpose:      (tbd) under construction
-Python:       Version 3.9
-Authors:      Tayte Waterman
-Course:       CS445/545 - Machine Learning
-Assignment:   Group project
-Date:         11/27/22
-======================================================================
-"""
 #Imports =============================================================
-from data_management import Data
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 
 #Constants ===========================================================
 
@@ -20,53 +13,67 @@ from sklearn.ensemble import RandomForestClassifier
 
 #Main ----------------------------------------------------------------
 def main():
-    #Load training/test data
-    data = Data()
+    print('Importing data ...')
+    data = pd.read_csv('emails.csv')
+    features = list(data.head())[1:-2]
+    y = np.array(data)[:,-1].astype(int)
+    x = np.array(data)[:,1:-2]
 
-    #Train and compute results - Random Forest Classifier
-    model = RandomForestClassifier().fit(data.train.X,data.train.Y)
-    Y = model.predict(data.test.X)
-    accuracy = 100*model.score(data.test.X,data.test.Y)
+    print('Parsing training and test data ...')
+    x_train, x_test, y_train, y_test = train_test_split(x,y)
 
-    #Separate data into Malignent and Benign predicted datasets
-    M = [[],[]]
-    B = [[],[]]
-    for i in range(len(Y)):
-        if Y[i] == 1:
-            M[0].append(data.test.tX[0][i])
-            M[1].append(data.test.tX[1][i])
-        else:
-            B[0].append(data.test.tX[0][i])
-            B[1].append(data.test.tX[1][i])
+    """
+    #Test performance over tree depth
+    e_train = []
+    e_test = []
+    nodes = [2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,200,300,400,500]
 
-    #Plot data (projected to 2D)
-    plt.figure("Breast Cancer Classification",figsize=(8,5))
-    plt.title("Breast Cancer - Random Forest Classifier")
+    for node in nodes:
+        model = DecisionTreeClassifier(max_leaf_nodes=node)
+        model.fit(x_train,y_train)
+        e_train.append(model.score(x_train,y_train))
+        e_test.append(model.score(x_test,y_test))
 
-    #Plot training data
-    plt.scatter(data.train.tXb[0],data.train.tXb[1],
-                c="#cccccc",s=10,label="Training - Benign")
-    plt.scatter(data.train.tXm[0],data.train.tXm[1],
-                c="#777777",marker="X",s=30,
-                label="Training - Malignent")
-
-    #Plot prediction results over test data
-    plt.scatter(B[0],B[1],
-                c="blue",s=15,label="Predicted - Benign")    
-    plt.scatter(M[0],M[1],
-                c="red",marker="X",s=40,
-                label="Predicted - Malignent")
-
-    #Append accuracy
-    plt.gcf().text(0.74,0.70,
-                   "Accuracy = " + str(round(accuracy,2)) + "%")
-
-    #Legend
-    plt.legend(bbox_to_anchor=(1.05, 1.0), loc='upper left')
-    plt.tight_layout()
-
-    #Show
+    plt.plot(nodes,e_train,c='red')
+    plt.plot(nodes,e_test,c='blue')
     plt.show()
+    """
+
+    #Initial alphas calculation
+    model = DecisionTreeClassifier()
+    model.fit(x_train,y_train)
+
+    ccp = model.cost_complexity_pruning_path(x_train,y_train)
+    alphas = ccp.ccp_alphas
+
+    e_train = []
+    e_test = []
+    i = 0
+    for alpha in alphas:
+        print(str(i)+' of '+str(len(alphas)))
+        i += 1
+        
+        model = DecisionTreeClassifier(ccp_alpha=alpha)
+        model.fit(x_train,y_train)
+        e_train.append(model.score(x_train,y_train))
+        e_test.append(model.score(x_test,y_test))
+
+    plt.plot(alphas,e_train,c='red')
+    plt.plot(alphas,e_test,c='gray')
+    plt.show()
+
+    """
+    #Train and plot model
+    print('Training model ...')
+    model = DecisionTreeClassifier(max_depth=5)
+    model.fit(x_train,y_train)
+    acc = model.score(x_test,y_test)
+    print(acc)
+
+    plt.figure()
+    tree.plot_tree(model,feature_names=features,filled=True)
+    plt.show()
+    """
     
 #Execute Main ========================================================
 if __name__ == "__main__":
